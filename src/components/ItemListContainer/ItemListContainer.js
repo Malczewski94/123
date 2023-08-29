6fe1aab9673c5f3f3562d733fe2d1e8fcf4eab08
Malczewski94 <tonnymalczewski01@gmail.com>
2023-08-29 06:13:39 -0300

    backup

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,23 +1,36 @@
 import { useState, useEffect } from "react";
-import { getProducts, getProductByCategory} from "../../asyncMock";
 import ItemList from "../ItemList/ItemList";
 import { useParams } from "react-router-dom";
+import { getDocs, collection, query, where} from "firebase/firestore";
+import {db} from "../../services/firebase/firebaseConfig"
 
 const ItemListContainer = ({greeting}) => {
     const [products, setProducts] = useState([])
+    const [loanding, setLoanding] = useState(true)
 
     const {categoryId} = useParams()
 
     useEffect(() => {
-        const asyncFunc = categoryId ? getProductByCategory : getProducts;
+        setLoanding(true)
 
-        asyncFunc(categoryId)
-            .then(response => {
-                setProducts(response)
+        const collectionRef = categoryId
+            ? query(collection(db, 'products'), where('category', '==', categoryId))
+            : collection(db, 'products')
+
+        getDocs(collectionRef)
+            .then(response =>{
+                const productsAdapted = response.docs.map(doc => {
+                    const data = doc.data()
+                    return {id: doc.id, ...data}
+                })
+                setProducts(productsAdapted)
             })
-            .catch(error => {
-                console.error(error)
+            .catch(error =>{
+                console.log(error)
             })
+            .finally(() =>{
+                setLoanding(false)
+            },1000)
     }, [categoryId])
 
     return (
---
+++ src/services/firebase/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAX4g8uts7IotkaVWEqFYInyVQFShhjPfM",
+  authDomain: "proyectofinal-react1.firebaseapp.com",
+  projectId: "proyectofinal-react1",
+  storageBucket: "proyectofinal-react1.appspot.com",
+  messagingSenderId: "949587388583",
+  appId: "1:949587388583:web:6cfd9d0986767ff8491382"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
